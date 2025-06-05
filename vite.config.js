a8f599fc82c7518c6dbf162f72afb95a0f189a32
Diaz Leonata <diazleonata@outlook.com>
2025-06-05 10:49:00 +0000

    update tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: [
       "35de4f89-0a0e-485f-84ea-6150faec2a08-00-10y9rnbeegl6c.pike.replit.dev",
